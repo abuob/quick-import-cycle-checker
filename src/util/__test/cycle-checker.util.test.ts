98dc1f95d06f8e619a130f0804dc700a6c8f9a1c
Sébastien Santschi <github@santschi.dev>
2022-07-07 15:44:46 +0000

    added test to ci; fixed test;

--- src/util/__test/cycle-checker.util.test.ts
+++ src/util/__test/cycle-checker.util.test.ts
@@ -3,13 +3,13 @@ import * as path from 'path';
 import { CycleCheckerUtil } from '../cycle-checker.util';
 
 describe('CycleCheckerUtil', () => {
-    it('should detect a simple cycle', () => {
+    it('should detect a simple cycle', async () => {
         const graphCreator: GraphCreator = GraphCreator.builder()
             .withRepoRoot(path.join(__dirname, './fixtures'))
             .withDirectoryToCheck(path.join(__dirname, './fixtures'))
             .build();
 
-        const graphWithCycle = graphCreator.createGraphForDir();
-        expect(CycleCheckerUtil.checkForCycles(graphWithCycle)).toThrow();
+        const graphWithCycle = await graphCreator.createGraphForDir();
+        expect(() => CycleCheckerUtil.checkForCycles(graphWithCycle)).toThrow();
     });
 });
