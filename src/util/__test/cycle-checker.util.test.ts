00cc6867bd46a176e5b440b86794c161210ee0f6
Sébastien Santschi <github@santschi.dev>
2022-07-09 17:22:16 +0000

    split tests

--- src/util/__test/cycle-checker.util.test.ts
+++ src/util/__test/cycle-checker.util.test.ts
@@ -3,28 +3,54 @@ import * as path from 'path';
 import { CycleCheckerUtil } from '../cycle-checker.util';
 
 describe('CycleCheckerUtil', () => {
-    const tests: [string, string, boolean][] = [
-        ['simple-cycle', 'should detect a simple cycle', true],
-        ['no-cycle-simple', 'should detect no cycle', false],
-        ['no-cycle-with-subdir', 'should detect no cycle and handle subdir', false],
-        ['simple-cycle-with-subdir', 'should detect a simple cycle and handle subdir', true],
-        ['handle-import-with-doublequotes', 'should detect a simple cycle and handle doublequotes gracefully', true]
-    ];
 
-    tests.forEach(([filePath, description, throws]) => {
-        const directory = `./fixtures/${filePath}`;
-        it(description, async () => {
-            const graphCreator: GraphCreator = GraphCreator.builder()
-                .withRepoRoot(path.join(__dirname, directory))
-                .withDirectoryToCheck(path.join(__dirname, directory))
-                .build();
+    it('should detect a simple cycle and handle subdir', async () => {
+        const graphCreator: GraphCreator = GraphCreator.builder()
+            .withRepoRoot(path.join(__dirname, './fixtures/simple-cycle-with-subdir'))
+            .withDirectoryToCheck(path.join(__dirname, './fixtures/simple-cycle-with-subdir'))
+            .build();
 
-            const testGraph = await graphCreator.createGraphForDir();
-            if (throws) {
-                expect(() => CycleCheckerUtil.checkForCycles(testGraph)).toThrow();
-            } else {
-                expect(() => CycleCheckerUtil.checkForCycles(testGraph)).not.toThrow();
-            }
-        });
+        const graphWithCycle = await graphCreator.createGraphForDir();
+        expect(() => CycleCheckerUtil.checkForCycles(graphWithCycle)).toThrow();
+    });
+
+    it('should detect a simple cycle and handle doublequotes gracefully', async () => {
+        const graphCreator: GraphCreator = GraphCreator.builder()
+            .withRepoRoot(path.join(__dirname, './fixtures/handle-import-with-doublequotes'))
+            .withDirectoryToCheck(path.join(__dirname, './fixtures/handle-import-with-doublequotes'))
+            .build();
+
+        const graphWithCycle = await graphCreator.createGraphForDir();
+        expect(() => CycleCheckerUtil.checkForCycles(graphWithCycle)).not.toThrow();
+    });
+
+    it('should detect no cycle', async () => {
+        const graphCreator: GraphCreator = GraphCreator.builder()
+            .withRepoRoot(path.join(__dirname, './fixtures/no-cycle-simple'))
+            .withDirectoryToCheck(path.join(__dirname, './fixtures/no-cycle-simple'))
+            .build();
+
+        const graphWithCycle = await graphCreator.createGraphForDir();
+        expect(() => CycleCheckerUtil.checkForCycles(graphWithCycle)).not.toThrow();
+    });
+
+    it('should detect no cycle and handle subdir', async () => {
+        const graphCreator: GraphCreator = GraphCreator.builder()
+            .withRepoRoot(path.join(__dirname, './fixtures/no-cycle-with-subdir'))
+            .withDirectoryToCheck(path.join(__dirname, './fixtures/no-cycle-with-subdir'))
+            .build();
+
+        const graphWithCycle = await graphCreator.createGraphForDir();
+        expect(() => CycleCheckerUtil.checkForCycles(graphWithCycle)).not.toThrow();
+    });
+
+    it('should detect a simple cycle', async () => {
+        const graphCreator: GraphCreator = GraphCreator.builder()
+            .withRepoRoot(path.join(__dirname, './fixtures/simple-cycle'))
+            .withDirectoryToCheck(path.join(__dirname, './fixtures/simple-cycle'))
+            .build();
+
+        const graphWithCycle = await graphCreator.createGraphForDir();
+        expect(() => CycleCheckerUtil.checkForCycles(graphWithCycle)).toThrow();
     });
 });
