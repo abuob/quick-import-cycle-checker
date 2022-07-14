06d51ea206dc53feea3806410d2a915e6700d8d4
Sébastien Santschi <github@santschi.dev>
2022-07-14 15:24:05 +0000

    added config flags to cli

--- bin.ts
+++ bin.ts
@@ -2,12 +2,25 @@
 
 import { QuickImportCycleChecker } from './src/quick-import-cycle-checker';
 
-// TODO Make this more elegant/configurable
-const directoryToCheck = process.cwd();
-const rootDirectory = process.cwd();
+export interface cliSettings {
+    checkDirectories: string[];
+    root: string;
+    exclusions: RegExp[];
+}
 
-QuickImportCycleChecker.forDirectories(directoryToCheck)
-    .withRootDirectory(rootDirectory)
+const settings: cliSettings = {
+    checkDirectories: process.env.npm_config_checkdirectories?.split(',') ?? [process.cwd()],
+    root: process.env.npm_config_root ?? process.cwd(),
+    exclusions: process.env.npm_config_exclusions
+        ? process.env.npm_config_exclusions?.split(',').map((str: string): RegExp => {
+              return new RegExp(str);
+          })
+        : []
+};
+
+QuickImportCycleChecker.forDirectories(...settings.checkDirectories)
+    .withRootDirectory(settings.root)
+    .withExclusions(settings.exclusions)
     .createImportGraph()
     .searchForImportCycles()
     .reportCyclesAndExit();
