5ecc3ab5301b3f5a8fc6c55ffd38248964e8f5b5
Sébastien Santschi <github@santschi.dev>
2022-07-16 20:48:04 +0000

    rewritten using reduce

--- src/util/parse-arguments.util.ts
+++ src/util/parse-arguments.util.ts
@@ -1,28 +1,15 @@
-export function parseArgs(argv: string[]): Record<string, string[]> {
-    const parsed: Record<string, string[]> = {};
+export function parseArgs(args: string[]): Record<string, string[]> {
+    let currentKey: string = '';
+    const emptyCliSettings: Record<string, string[]> = {};
 
-    const args: string[] = argv.slice(2);
-    const indicesOfFlags: number[] = [];
-
-    for (let i = 0; i < args.length; i++) {
-        const res = args.slice(i).findIndex((value: string): boolean => {
-            return /^--.*/.test(value);
-        });
-        if (res >= 0) {
-            indicesOfFlags.push(res + i);
-            i += res;
+    return args.reduce((prev: Record<string, string[]>, curr: string): Record<string, string[]> => {
+        if (/^--.*/.test(curr)) {
+            currentKey = curr;
+            return prev;
         }
-    }
-
-    indicesOfFlags.forEach((flagIndexInArgs: number, indexInIndices: number): void => {
-        const nextFlagIndexInArgs = indicesOfFlags[indexInIndices + 1] || args.length;
-        if (flagIndexInArgs + 1 >= nextFlagIndexInArgs) {
-            // Boolean Flag
-            parsed[args[flagIndexInArgs]] = ['true'];
-        } else {
-            parsed[args[flagIndexInArgs]] = args.slice(flagIndexInArgs + 1, nextFlagIndexInArgs);
-        }
-    });
-
-    return parsed;
+        return {
+            ...prev,
+            [currentKey]: [...(prev[currentKey] ?? []), curr]
+        };
+    }, emptyCliSettings);
 }
